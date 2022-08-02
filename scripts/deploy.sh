6e50f26e87c36a3f852902b7dea579d33d3ea8fc
Inha Yoon <survedog@gmail.com>
2022-08-02 22:34:54 +0900

    Set datadog agent to monitor application log file

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -20,4 +20,4 @@ echo "> $JAR_NAME에 실행권한 추가"
 chmod +x $JAR_NAME
 
 echo "> $JAR_NAME 실행"
-nohup /opt/jdk-17/bin/java -jar -Dspring.profiles.active=server $JAR_NAME > $REPOSITORY/nohup.out &
+nohup /opt/jdk-17/bin/java -jar -Dspring.profiles.active=server $JAR_NAME > $REPOSITORY/nohup.out 2>&1 &
--- src/main/java/com/example/test2/controller/TestController.java
+++ src/main/java/com/example/test2/controller/TestController.java
@@ -3,6 +3,8 @@ package com.example.test2.controller;
 import com.example.test2.dto.PersonDTO;
 import com.example.test2.service.TestService;
 import lombok.RequiredArgsConstructor;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -12,32 +14,38 @@ import java.util.List;
 public class TestController {
 
     private final TestService testService;
+    private final Logger log = LoggerFactory.getLogger(this.getClass());
 
     @PostMapping("/save")
     public String save(@RequestBody PersonDTO dto) {
+        log.info("Called save()");
         testService.save(dto);
         return "save Success";
     }
 
     @PostMapping("/saveAll")
     public String saveAll(@RequestBody PersonDTO[] dto) {
+        log.info("Called saveAll()");
         testService.saveAll(dto);
         return "saveAll Success";
     }
 
     @GetMapping("/find")
     public PersonDTO find(@RequestBody String name) {
+        log.info("Called find()");
         return testService.find(name);
     }
 
     @GetMapping("/findAll")
     public List<PersonDTO> findAll() {
+        log.info("Called findAll()");
         return testService.findAll();
     }
 
     @GetMapping("/get")
     @ResponseBody
     public String getFixedData() {
+        log.info("Called get()");
         return " 고정 데이터";
     }
 }
